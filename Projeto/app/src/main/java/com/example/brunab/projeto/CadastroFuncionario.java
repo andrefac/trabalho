e837a054867a57aebf6835bbab58e21a6a39f78a
brunabertolo <bertolobruna@gmail.com>
2018-11-16 19:59:00 -0300

    Cadastro Funcionario

--- Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
@@ -1,14 +1,13 @@
-
 package com.example.brunab.projeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CadastroCarro extends AppCompatActivity {
+public class CadastroFuncionario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cdastro_carro);
+        setContentView(R.layout.activity_cadastro_funcionario);
     }
 }
