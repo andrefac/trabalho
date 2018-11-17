829de041a8de0a86f4122b7d2d08953441a06148
Andre <andrefsalmeida@gmail.com>
2018-11-17 16:09:13 -0300

    Ajuste na activity Locacao e criação das funçoes para chamar as activity CArro e Funcionario

--- Projeto/app/src/main/java/com/example/brunab/projeto/CadastroCarro.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/CadastroCarro.java
@@ -4,13 +4,26 @@ package com.example.brunab.projeto;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
 public class CadastroCarro extends AppCompatActivity {
+
+    private EditText vPlaca;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cdastro_carro);
+
+        vPlaca = findViewById(R.id.editPlaca);
+
+        SimpleMaskFormatter smfPlaca = new SimpleMaskFormatter("LLL-NNNN");
+        MaskTextWatcher mtwPlaca = new MaskTextWatcher(vPlaca, smfPlaca);
+        vPlaca.addTextChangedListener(mtwPlaca);
     }
 
     public void voltar (View view){
@@ -18,6 +31,6 @@ public class CadastroCarro extends AppCompatActivity {
     }
 
     public void salvar (View view){
-        finish();
+        //finish();
     }
 }
