57fef60cd711c8cdc615877c57c992e2c3518386
brunabertolo <bertolobruna@gmail.com>
2018-11-22 20:38:14 -0300

    Sincronizar

--- Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class CadastroFuncionario extends AppCompatActivity {
+public class
+CadastroFuncionario extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
