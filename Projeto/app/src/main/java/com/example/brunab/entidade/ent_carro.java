4c4f3fc21509748538c61a55d6259bf5de570c24
brunabertolo <bertolobruna@gmail.com>
2018-11-22 22:22:38 -0300

    atualização. Criação da classe Salvar

---
+++ Projeto/app/src/main/java/com/example/brunab/entidade/ent_carro.java
@@ -0,0 +1,50 @@
+package com.example.brunab.entidade;
+
+public class ent_carro {
+
+    private String marca;
+    private String modelo;
+    private String placa;
+    private String vLocacao;
+    private String vSeguro;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getvLocacao() {
+        return vLocacao;
+    }
+
+    public void setvLocacao(String vLocacao) {
+        this.vLocacao = vLocacao;
+    }
+
+    public String getvSeguro() {
+        return vSeguro;
+    }
+
+    public void setvSeguro(String vSeguro) {
+        this.vSeguro = vSeguro;
+    }
+}
