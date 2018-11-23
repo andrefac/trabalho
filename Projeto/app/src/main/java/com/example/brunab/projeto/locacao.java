f224142b15598d9324bf1d13d7cac6a1e9256469
CassiaSotolani <ca.sotolani@gmail.com>
2018-11-22 22:13:08 -0300

    atualização com os values

--- Projeto/app/src/main/java/com/example/brunab/projeto/locacao.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/locacao.java
@@ -1,10 +1,14 @@
 package com.example.brunab.projeto;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.example.brunab.entidade.ent_locacao;
 
 public class locacao extends AppCompatActivity {
 
@@ -15,7 +19,8 @@ public class locacao extends AppCompatActivity {
     private EditText locaDataDev;
     private EditText locaKmFinal;
     private EditText locaValor;
-
+    private SQLiteDatabase conexao;
+    db_class banco;
 
 
     @Override
@@ -36,7 +41,39 @@ public class locacao extends AppCompatActivity {
 
     public void acSalvaLocacao (View view){
 
-        
+        banco = new db_class(this);
+
+        ent_locacao locacao = new ent_locacao();
+        locacao.setLocaNome(locaNome.getText().toString());
+        locacao.setLocaPlaca(locaPlaca.getText().toString());
+        locacao.setLocaKmInicial(locaKmInicial.getText().toString());
+        locacao.setLocaDataLoca(locaDataLoca.getText().toString());
+        locacao.setLocaDataDev(locaDataDev.getText().toString());
+        locacao.setLocaKmFinal(locaKmFinal.getText().toString());
+        locacao.setLocaValor(locaValor.getText().toString());
+
+
+
+
+        try {
+
+            conexao = banco.getReadableDatabase();
+            ContentValues values = new ContentValues();
+
+            values.put("n_cliente", locacao.getLocaNome());
+            values.put("placa_carro", locacao.getLocaPlaca());
+            values.put("dt_loca", locacao.getLocaDataLoca());
+            values.put("km_ini", locacao.getLocaKmInicial());
+            values.put("dt_devo", locacao.getLocaDataDev());
+            values.put("km_fim", locacao.getLocaKmFinal());
+            values.put("valor_total", locacao.getLocaValor());
+
+
+        }catch (Exception e){
+
+            
+
+        }
 
     }
 
