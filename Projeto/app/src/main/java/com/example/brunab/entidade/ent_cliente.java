360ff63c6b74ce1f568a8f31332fb67d2f0b3483
Andre Castro <andrefsalmeida@gmail.com>
2018-11-22 22:29:40 -0300

    Criei a classe entidade do cliente, mapeei os campos, criei a ação de salvar do botao da tela de cliente

--- Projeto/app/src/main/java/com/example/brunab/entidade/ent_cliente.java
+++ Projeto/app/src/main/java/com/example/brunab/entidade/ent_cliente.java
@@ -2,5 +2,58 @@ package com.example.brunab.entidade;
 
 public class ent_cliente {
 
-    private String vNome;
+    private String cNome;
+    private String cCpf;
+    private String cRg;
+    private String cCnh;
+    private String cDependente;
+    private String cEndereco;
+
+    public String getcNome() {
+        return cNome;
+    }
+
+    public void setcNome(String cNome) {
+        this.cNome = cNome;
+    }
+
+    public String getcCpf() {
+        return cCpf;
+    }
+
+    public void setcCpf(String cCpf) {
+        this.cCpf = cCpf;
+    }
+
+    public String getcRg() {
+        return cRg;
+    }
+
+    public void setcRg(String cRg) {
+        this.cRg = cRg;
+    }
+
+    public String getcCnh() {
+        return cCnh;
+    }
+
+    public void setcCnh(String cCnh) {
+        this.cCnh = cCnh;
+    }
+
+    public String getcDependente() {
+        return cDependente;
+    }
+
+    public void setcDependente(String cDependente) {
+        this.cDependente = cDependente;
+    }
+
+    public String getcEndereco() {
+        return cEndereco;
+    }
+
+    public void setcEndereco(String cEndereco) {
+        this.cEndereco = cEndereco;
+    }
 }
--- Projeto/app/src/main/java/com/example/brunab/projeto/cadastro_cliente.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/cadastro_cliente.java
@@ -1,9 +1,13 @@
 package com.example.brunab.projeto;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import com.example.brunab.entidade.ent_cliente;
 
 public class cadastro_cliente extends AppCompatActivity {
 
@@ -13,6 +17,8 @@ public class cadastro_cliente extends AppCompatActivity {
     private EditText cCnh;
     private EditText cDependente;
     private EditText cEndereco;
+    db_class banco;
+    private SQLiteDatabase conexao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,34 @@ public class cadastro_cliente extends AppCompatActivity {
 
     public void acSalvaCliente (View view){
 
+        banco = new db_class(this);
+
+
+        ent_cliente cliente = new ent_cliente();
+        cliente.setcNome(cNome.getText().toString());
+        cliente.setcCpf(cCpf.getText().toString());
+        cliente.setcRg(cRg.getText().toString());
+        cliente.setcCnh(cCnh.getText().toString());
+        cliente.setcDependente(cDependente.getText().toString());
+        cliente.setcEndereco(cEndereco.getText().toString());
+
+
+        try {
+            conexao = banco.getWritableDatabase();
+            ContentValues values = new ContentValues();
+            values.put("cpf", cliente.getcCpf());
+            values.put("nome", cliente.getcNome());
+            values.put("rg", cliente.getcRg());
+            values.put("cnh", cliente.getcCnh());
+            values.put("n_dependentes", cliente.getcDependente());
+            values.put("endereco", cliente.getcEndereco());
+
+            conexao.insert("Cliente", null, values);
+            conexao.close();
+
+        } catch (Exception e){
+
+        }
     }
 
     public void acCancelar (View view){
