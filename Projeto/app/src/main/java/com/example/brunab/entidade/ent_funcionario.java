59280fdda7aef1586406259ffe7d1eaab33612ba
Aviney Davi <avineydavi@gmail.com>
2018-11-23 22:38:48 -0300

    Criação da tela de cadastro funcionario, botão sair e salvar e criação da class entidade

---
+++ Projeto/app/src/main/java/com/example/brunab/entidade/ent_funcionario.java
@@ -0,0 +1,68 @@
+package com.example.brunab.entidade;
+
+public class ent_funcionario {
+
+    private String fun_nome;
+    private String fun_rg;
+    private String fun_cpf;
+    private String fun_cargo;
+    private String fun_end;
+    private String fun_date_adm;
+    private String fun_date_dms;
+
+    public String getFun_nome() {
+        return fun_nome;
+    }
+
+    public void setFun_nome(String fun_nome) {
+        this.fun_nome = fun_nome;
+    }
+
+    public String getFun_rg() {
+        return fun_rg;
+    }
+
+    public void setFun_rg(String fun_rg) {
+        this.fun_rg = fun_rg;
+    }
+
+    public String getFun_cpf() {
+        return fun_cpf;
+    }
+
+    public void setFun_cpf(String fun_cpf) {
+        this.fun_cpf = fun_cpf;
+    }
+
+    public String getFun_cargo() {
+        return fun_cargo;
+    }
+
+    public void setFun_cargo(String fun_cargo) {
+        this.fun_cargo = fun_cargo;
+    }
+
+    public String getFun_end() {
+        return fun_end;
+    }
+
+    public void setFun_end(String fun_end) {
+        this.fun_end = fun_end;
+    }
+
+    public String getFun_date_adm() {
+        return fun_date_adm;
+    }
+
+    public void setFun_date_adm(String fun_date_adm) {
+        this.fun_date_adm = fun_date_adm;
+    }
+
+    public String getFun_date_dms() {
+        return fun_date_dms;
+    }
+
+    public void setFun_date_dms(String fun_date_dms) {
+        this.fun_date_dms = fun_date_dms;
+    }
+}
--- Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
+++ Projeto/app/src/main/java/com/example/brunab/projeto/CadastroFuncionario.java
@@ -1,23 +1,79 @@
 package com.example.brunab.projeto;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.brunab.entidade.ent_funcionario;
 
 public class
 CadastroFuncionario extends AppCompatActivity {
+
+    private EditText fun_nome;
+    private EditText fun_rg;
+    private EditText fun_cpf;
+    private EditText fun_cargo;
+    private EditText fun_end;
+    private EditText fun_date_adm;
+    private EditText fun_date_dms;
+
+    private SQLiteDatabase conexao;
+    db_class banco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_funcionario);
+
+        fun_nome = findViewById(R.id.fun_nome);
+        fun_rg = findViewById(R.id.fun_rg);
+        fun_cpf = findViewById(R.id.fun_cpf);
+        fun_cargo = findViewById(R.id.fun_cargo);
+        fun_end = findViewById(R.id.fun_end);
+        fun_date_adm = findViewById(R.id.fun_date_adm);
+        fun_date_dms = findViewById(R.id.fun_date_dms);
+
+
     }
 
-    public void voltar (View view){
+
+    public void btVoltar (View view){
         finish();
     }
 
-    public void salvar (View view){
-        finish();
+    public void btSalvar (View view){
+
+        banco = new db_class(this);
+
+        ent_funcionario funcionario = new ent_funcionario();
+        funcionario.setFun_nome(fun_nome.getText().toString());
+        funcionario.setFun_rg(fun_rg.getText().toString());
+        funcionario.setFun_cpf(fun_cpf.getText().toString());
+        funcionario.setFun_cargo(fun_cargo.getText().toString());
+        funcionario.setFun_end(fun_end.getText().toString());
+        funcionario.setFun_date_adm(fun_date_adm.getText().toString());
+        funcionario.setFun_date_dms(fun_date_dms.getText().toString());
+
+    try {
+        conexao = banco.getReadableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("cpf", funcionario.getFun_cpf());
+        values.put("nome", funcionario.getFun_nome());
+        values.put("rg", funcionario.getFun_rg());
+        values.put("endereco", funcionario.getFun_end());
+        values.put("cargo", funcionario.getFun_cargo());
+        values.put("dt_adm", funcionario.getFun_date_adm());
+       // values.put("dt_contr", funcionario.getFun_date_adm());
+        values.put("dt_demi", funcionario.getFun_date_dms());
+
+
+
+
+    }catch (Exception e){}
+
     }
 }
